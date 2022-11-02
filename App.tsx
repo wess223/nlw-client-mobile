443d402f6dac11b738e172c93004710c9962aa84
Wesley Alves <wesley.info07@gmail.com>
2022-11-02 15:42:22 -0300

    adicionando loading

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor='gray.400'>
-        <Text color='white' fontSize={24}>
-          Hello React Native!
-        </Text>
-        <StatusBar style="auto" />
+      <Center flex={1} bgColor='gray.900'>
+        {
+          fontsLoaded ? <SignIn /> : <Loading />
+        }
       </Center>
     </NativeBaseProvider>
   );
