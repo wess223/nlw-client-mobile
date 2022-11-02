1e7b60c73a7c67f5e6aab4d5c119c04bd94e3e00
Wesley Alves <wesley.info07@gmail.com>
2022-11-02 14:48:57 -0300

    adicionando assets

--- App.tsx
+++ App.tsx
@@ -4,17 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//RN trabalha com independencia de pixel
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#FFF',
+    fontSize: 24
+  }
 });
