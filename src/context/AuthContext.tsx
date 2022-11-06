deebd54e970f4012bb397a2e42983d5842cc7be8
Wesley Alves <wesley.info07@gmail.com>
2022-11-06 18:07:36 -0300

    configurando e logando na aplicação, gerando token de autenticação.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -8,6 +14,7 @@ interface UserProps {
 export interface AuthContextDataProps {
     user: UserProps;
     signIn: () => Promise<void>;
+    isUserLoading: boolean;
 }
 
 interface AuthProviderProps {
@@ -18,17 +25,43 @@ export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
 
+    const [user, setUser] = useState<UserProps>({} as UserProps);
+    const [isUserLoading, setIsUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '724592702756-ckrel592d0cjffu0ph3mk0k4n63trif1.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email'] //escopo do usuário que tenho acesso na api google
+    })
+
     async function signIn() {
-        console.log('vamos logar');
+        try {
+            setIsUserLoading(true)
+            await promptAsync(); //starta o fluxo de autenticação.
+        } catch (err) {
+            console.log(err);
+            throw err;
+
+        } finally {
+            setIsUserLoading(false)
+        }
     }
+
+    async function signInWithGoogle(access_token: string) {
+        console.log("TOKEN DE AUTENTICAÇÃO ======>", access_token)
+    }
+
+    useEffect(() => {
+        if (response?.type === 'success' && response.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken)
+        }
+    }, [response])
 
     return (
         <AuthContext.Provider value={{
             signIn,
-            user: {
-                name: 'Wesley Alves',
-                avatarUrl: 'https://github.com/wess223.png'
-            }
+            isUserLoading,
+            user
         }}>
             {children}
         </AuthContext.Provider>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -10,18 +10,16 @@ import { useAuth } from '../hooks/useAuth'
 export function SignIn() {
     const { signIn, user } = useAuth();
 
-    console.log('dados do usuario', user);
-    
-
     return (
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
 
             <Button
                 type="SECONDARY"
-                title='Entrar com o google.'
+                title="Entrar com o google"
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
                 mt={12}
+                onPress={signIn}
             />
 
             <Text color="white" mt={4}>
